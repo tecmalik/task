2eb5059150177ad0f82fd2290267a97e099c906e
tecmalik <abdulmalikojo2@gmail.com>
2025-06-21 13:35:43 +0100

    commiting number readertask

--- dailytasks/dailyTask/src/tasks/TimeTeller.java
+++ dailytasks/dailyTask/src/tasks/TimeTeller.java
@@ -4,15 +4,46 @@ public class TimeTeller {
     static String errorMessage = "wrong time format";
     static String Prefix = "The Time is ";
     static String Postfix = " O'clock";
+    static String abbreviation = "";
+
     public static String tellTheTime(String time) {
+
         String spacelessTime = time.replaceAll("\\s","");
         String[] digits = spacelessTime.split(":");
         NumberReader numberReader = new NumberReader();
         long hour =  Long.parseLong(digits[0]);
-        long minute =  Long.parseLong(digits[1]);
-        if (hour>12 || String.valueOf(hour).length() > 2)return errorMessage;
-        if (minute>59 || String.valueOf(minute).length() > 2)return errorMessage;
-        if( minute == 0L) return Prefix.concat(numberReader.readNumber(hour).concat(Postfix)) ;
+        long minute = checkForAbbreviation(digits);
+
+
+        if (isNotValidFormat(hour, digits))return errorMessage;
+        if( minute == 0L) return Prefix.concat(numberReader.readNumber(hour).concat(Postfix)).concat(abbreviation) ;
     return "";
+    }
+
+    private static boolean isNotValidFormat(long hour, String[] digits) {
+        return hour < 1 || hour > 12 && !digits[1].matches("^(?:(?:[0-5]?\\d|60)(?i)(am|pm)|(?:[0-5]?\\d|60))$");
+    }
+
+    private static boolean isABoolean(String[] digits) {
+        return digits[1].length() > 2;
+    }
+
+    private static long checkForAbbreviation(String[] digits) {
+
+        if (digits[1].matches("(?i).*(am|pm).*")){
+            identifyAbbreviation(digits[1]);
+            return Long.parseLong(removeAbbreviation(digits[1]));
+
+        }
+        return  Long.parseLong(digits[1]);
+    }
+
+    private static String removeAbbreviation(String albaNumeric) {
+        return albaNumeric.replaceAll("[a-zA-z]", "");
+    }
+
+    private static void identifyAbbreviation(String Alphanumeric) {
+        if (Alphanumeric.matches("(?i).*am.*")) abbreviation = " Am";
+        if (Alphanumeric.matches("(?i).*pm.*")) abbreviation = " Pm";
     }
 }
--- dailytasks/dailyTask/test/tasks/TimeTellerTest.java
+++ dailytasks/dailyTask/test/tasks/TimeTellerTest.java
@@ -1,31 +1,32 @@
 package tasks;
 
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimeTellerTest {
-    TimeTeller timeTeller;
-    @BeforeEach
-    public void setup() {
-        timeTeller = new TimeTeller();
-    }
 
     @Test
     public void TimeTellerCanTellTheCorrectTime() {
-        assertEquals("The Time is twelve O'clock", timeTeller.tellTheTime("12:00"));
-        assertEquals("The Time is one O'clock", timeTeller.tellTheTime("1:00"));
-        assertEquals("The Time is one O'clock", timeTeller.tellTheTime("01:00"));
+        assertEquals("The Time is twelve O'clock", TimeTeller.tellTheTime("12:00"));
+        assertEquals("The Time is one O'clock", TimeTeller.tellTheTime("1:00"));
+        assertEquals("The Time is one O'clock", TimeTeller.tellTheTime("01:00"));
 
 
     }
     @Test
-    public void TimeTellerCanTellTheTimeMeridiem(){
-        assertEquals("wrong time format", timeTeller.tellTheTime("13:00"));
-        assertEquals("wrong time format", timeTeller.tellTheTime("6:100"));
+    public void TimeTellerDoesNotAcceptInvalidTimeFormat(){
+        assertEquals("wrong time format", TimeTeller.tellTheTime("13:00"));
+        assertEquals("wrong time format", TimeTeller.tellTheTime("001:00"));
+        assertEquals("wrong time format", TimeTeller.tellTheTime("6:100"));
 
     };
+    @Test
+    public void TimeTellerCanBeToldWithAnteMeridiem(){
+        assertEquals("The Time is twelve O'clock Am",TimeTeller.tellTheTime("12:00 AM"));
+        assertEquals("The Time is twelve O'clock Am",TimeTeller.tellTheTime("12:00 Am"));
+        assertEquals("The Time is twelve O'clock Am",TimeTeller.tellTheTime("12:00Am"));
+
+    }
 
 }
