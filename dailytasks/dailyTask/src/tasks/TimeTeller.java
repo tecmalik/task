154132d66fa4c1c9f1aa20ff6f6b961b50ec675c
tecmalik <abdulmalikojo2@gmail.com>
2025-06-22 11:51:56 +0100

    commiting number readertask

--- dailytasks/dailyTask/src/tasks/TimeTeller.java
+++ dailytasks/dailyTask/src/tasks/TimeTeller.java
@@ -7,21 +7,26 @@ public class TimeTeller {
     static String abbreviation = "";
 
     public static String tellTheTime(String time) {
-
+        abbreviation = "";
+        if (time == null || time.trim().isEmpty()) {
+            return errorMessage;
+        }
         String spacelessTime = time.replaceAll("\\s","");
         String[] digits = spacelessTime.split(":");
         NumberReader numberReader = new NumberReader();
         long hour =  Long.parseLong(digits[0]);
         long minute = checkForAbbreviation(digits);
 
-
         if (isNotValidFormat(hour, digits))return errorMessage;
-        if( minute == 0L) return Prefix.concat(numberReader.readNumber(hour).concat(Postfix)).concat(abbreviation) ;
-    return "";
+        if( minute == 0L) return Prefix.concat(numberReader.readNumber(hour).concat(Postfix)) ;
+        if(minute > 0) return Prefix.concat(numberReader.readNumber(hour).concat(numberReader.readNumber(minute)).concat(Postfix)).concat(abbreviation) ;
+
+        return "";
+
     }
 
     private static boolean isNotValidFormat(long hour, String[] digits) {
-        return hour < 1 || hour > 12 && !digits[1].matches("^(?:(?:[0-5]?\\d|60)(?i)(am|pm)|(?:[0-5]?\\d|60))$");
+        return  !String.valueOf(hour).matches("0[0-9]|1[0-2]") || !digits[1].matches("^([0-5][0-9]|60)(?i)(am|pm)?$" );
     }
 
     private static long checkForAbbreviation(String[] digits) {
@@ -29,7 +34,6 @@ public class TimeTeller {
         if (digits[1].matches("(?i).*(am|pm).*")){
             identifyAbbreviation(digits[1]);
             return Long.parseLong(removeAbbreviation(digits[1]));
-
         }
         return  Long.parseLong(digits[1]);
     }
@@ -39,7 +43,7 @@ public class TimeTeller {
     }
 
     private static void identifyAbbreviation(String Alphanumeric) {
-        if (Alphanumeric.matches("(?i).*am.*")) abbreviation = " Am";
-        if (Alphanumeric.matches("(?i).*pm.*")) abbreviation = " Pm";
+        if (Alphanumeric.matches("^(\\d{2})(?i)am$")) abbreviation = " Am";
+        if (Alphanumeric.matches("^(\\d{2})(?i)pm$")) abbreviation = " Pm";
     }
 }
--- dailytasks/dailyTask/test/tasks/TimeTellerTest.java
+++ dailytasks/dailyTask/test/tasks/TimeTellerTest.java
@@ -9,7 +9,7 @@ public class TimeTellerTest {
     @Test
     public void TimeTellerCanTellTheCorrectTime() {
         assertEquals("The Time is twelve O'clock", TimeTeller.tellTheTime("12:00"));
-        assertEquals("The Time is one O'clock", TimeTeller.tellTheTime("1:00"));
+        assertEquals("The Time is four O'clock", TimeTeller.tellTheTime("04:00"));
         assertEquals("The Time is one O'clock", TimeTeller.tellTheTime("01:00"));
 
 
@@ -17,6 +17,7 @@ public class TimeTellerTest {
     @Test
     public void TimeTellerDoesNotAcceptInvalidTimeFormat(){
         assertEquals("wrong time format", TimeTeller.tellTheTime("13:00"));
+        assertEquals("wrong time format", TimeTeller.tellTheTime("24:00"));
         assertEquals("wrong time format", TimeTeller.tellTheTime("001:00"));
         assertEquals("wrong time format", TimeTeller.tellTheTime("6:100"));
 
